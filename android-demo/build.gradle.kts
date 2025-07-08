a823d43bebba5b8b07df7d7d276722fb8d294a25
xiaqiulei <xiaqiulei@bilibili.com>
2025-07-08 12:16:57 +0800

    update

--- android-demo/build.gradle.kts
+++ android-demo/build.gradle.kts
@@ -5,3 +5,36 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.jvm) apply false
 }
 
+subprojects {
+    plugins.withId("com.android.application") {
+        configure<com.android.build.gradle.AppExtension> {
+            compileSdkVersion(35)
+            defaultConfig {
+                minSdk = 24
+                targetSdk = 35
+            }
+            compileOptions {
+                sourceCompatibility = JavaVersion.VERSION_11
+                targetCompatibility = JavaVersion.VERSION_11
+            }
+            // ndkVersion = "27.2.12479018"
+            ndkVersion = "28.1.13356709"
+        }
+    }
+
+    plugins.withId("com.android.library") {
+        configure<com.android.build.gradle.LibraryExtension> {
+            compileSdkVersion(35)
+            defaultConfig {
+                minSdk = 24
+                targetSdk = 35
+            }
+            compileOptions {
+                sourceCompatibility = JavaVersion.VERSION_11
+                targetCompatibility = JavaVersion.VERSION_11
+            }
+            // ndkVersion = "27.2.12479018"
+            ndkVersion = "28.1.13356709"
+        }
+    }
+}
